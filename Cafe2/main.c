a15333ce95a786e9a36b87047d6868640cab60a4
charlie38 <38466672+charlie38@users.noreply.github.com>
2018-04-19 12:54:49 +0000

    Update main.c

--- Cafe2/main.c
+++ Cafe2/main.c
@@ -3,5 +3,12 @@
 
 int main(int argc, char *argv){
     automate A;
-    
+    if (argc != 2){
+        printf("USAGE: ./test_automate nom_fichier");
+        exit(1);
+    }
+    char *nom_fichier = argv[1];
+    FILE *f = fopen(nom_fichier,"r");
+    lecture_automate(&A,f);
+    return 0;
 }
