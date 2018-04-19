a06866fe76e0124a03c291a8b13e5f4e7496a514
charlie38 <38466672+charlie38@users.noreply.github.com>
2018-04-19 12:54:59 +0000

    Update main.c

--- Cafe2/main.c
+++ Cafe2/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "automate.h
 
-int main(int argc, char *argv){
+int main(int argc, char *argv[]){
     automate A;
     if (argc != 2){
         printf("USAGE: ./test_automate nom_fichier");
